e4a39fa7983879e0ac882f9c185ba15298b52e41
kushvidov <kushvidov11@gmail.com>
2024-02-26 18:23:02 +0100

    secondhomework

---
+++ Lesson2/src/PrimitiveDataTapes.java
@@ -0,0 +1,77 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PrimitiveDataTapes {
+    public static void main(String[] args) {
+
+        byte b1 = 0B1100;
+        byte b2 = 014;
+        byte b3 = 12;
+        byte b4 = 0XC;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 0B010100010100;
+        short s2 = 02424;
+        short s3 = 1300;
+        short s4 = 0x514;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0B0;
+        int i2 = 00;
+        int i3 = 0;
+        int i4 = 0X0;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 0B0111010110111100110100010101;
+        long l2 = 0726746425;
+        long l3 = 123456789;
+        long l4 = 0x75BCD15;
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        float f1 = 3.35F;
+        float f2 = 2.42F;
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 7.56;
+        double d2 = 8.72;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+        char c1 = 'w';
+        char c2 = 't';
+        char c3 = 450;
+        char c4 = '\u1454';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+
+
+    }
+}
