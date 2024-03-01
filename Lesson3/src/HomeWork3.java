ab361b098105c9d836035c1f6a4f7c8dd4f7687f
kushvidov <kushvidov11@gmail.com>
2024-03-01 16:30:32 +0100

    HomeWork3

---
+++ Lesson3/src/HomeWork3.java
@@ -0,0 +1,20 @@
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+    int a = 5;
+    int result1 = a-- - --a + ++a + a++ + a;
+    int b = 8;
+    int result2= ++b - b++ + ++b - --b;
+        System.out.println(result1);
+        System.out.println(result2);
+
+    int i1 = 5;
+    int i2 = 11;
+    double d1 = 5.5;
+    double d2 = 1.3;
+    long l1 = 20l;
+    double result = 0;
+    result = i2/d1+d2%i1-l1;
+    System.out.println(result);
+  }
+}
